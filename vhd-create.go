cba5d0b52c5db49552d7d7ab8094a4479586e99c
Sergio Rubio <rubiojr@frameos.org>
2015-02-09 21:43:30 +0100

    Accept size and file name to create sparses

--- vhd-create.go
+++ vhd-create.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"./vhd"
+	"fmt"
+	"github.com/dustin/go-humanize"
+	"os"
 )
 
 func main() {
-	vhd.CreateSparseVHD(40*1024*1024, "test.vhd")
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: gh-vhd create <file> <size-in-bytes>\n")
+		os.Exit(1)
+	}
+
+	file := os.Args[1]
+	size := os.Args[2]
+
+	isize, err := humanize.ParseBytes(size)
+
+	if err != nil {
+		panic(err)
+	}
+
+	vhd.CreateSparseVHD(uint64(isize), file)
+	fmt.Printf("File %s (%s) created\n", file, humanize.IBytes(uint64(isize)))
 }
